f5544993cd81983b93edb3d22585a1c19ba757ab
goingobsolete <goingobsolete@gmail.com>
2024-10-08 13:30:45 +0200

    added imports to sharedDatajs and made test City array with words

---
+++ src/sharedData.js
@@ -0,0 +1,19 @@
+
+//import Font Files
+import CaslonDisplay from "src/fonts/Libre_Caslon_Display/LibreCaslonDisplay-Regular.ttf";
+import CaslonReg from "src/fonts/Libre_Caslon_Text/LibreCaslonText-Regular.ttf";
+import CaslonBold from "src/fonts/Libre_Caslon_Text/LibreCaslonText-Bold.ttf";
+import CaslonItalic from "src/fonts/Libre_Caslon_Text/LibreCaslonText-Italic.ttf";
+
+//import Image Files
+import bgPage from "src/images/pagetexture_cleaned.png";
+
+//import json files
+import testCityList from "src/jsons/testArray.json";
+
+//TODO: Add city list with just the names and longitudes work out how to re reset the date line?
+//TODO: am i picking the cities here in a function?
+
+
+ //define objects to hold assets
+ 
